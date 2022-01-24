858b7c476fa00c02cfffb7e07e37899fd202a276
adriavieira314 <adriavieira314@gmail.com>
2022-01-23 22:09:53 -0400

    conteudo de menu perfil adicionado

--- lib/pages/menu/profile/profile_screen.dart
+++ lib/pages/menu/profile/profile_screen.dart
@@ -10,8 +10,65 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Profile'),
+    return Padding(
+      padding: const EdgeInsets.only(left: 4.0),
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(top: 58.0, bottom: 50.0),
+            child: Text(
+              'Editar Perfil',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: Image.network('https://picsum.photos/id/237/80/80'),
+            ),
+          ),
+          Wrap(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                padding: const EdgeInsets.fromLTRB(30.0, 3.0, 30.0, 3.0),
+                child: const Text('Alexander Albuquerque'),
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 15.0, bottom: 15.0),
+            child: Text(
+              'Telefone',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Wrap(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                padding: const EdgeInsets.fromLTRB(30.0, 3.0, 30.0, 3.0),
+                child: const Text('(92) 999563-7896'),
+              ),
+            ],
+          ),
+          Container()
+        ],
+      ),
     );
   }
 }
