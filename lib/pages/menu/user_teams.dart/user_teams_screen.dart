5031c458f0f7c6f20139b717f089267cd14aad68
adriavieira314 <adriavieira731@gmail.com>
2022-05-02 10:24:14 -0400

    adicionado lista de usuários do time

--- lib/pages/menu/user_teams.dart/user_teams_screen.dart
+++ lib/pages/menu/user_teams.dart/user_teams_screen.dart
@@ -1,5 +1,6 @@
 import 'package:fennec_desktop/components/appbar.dart';
 import 'package:fennec_desktop/components/bottom_navigation_bar.dart';
+import 'package:fennec_desktop/models/list_of_users.dart';
 import 'package:fennec_desktop/models/team_list.dart';
 import 'package:fennec_desktop/services/team_list_dao.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,12 @@ class UserTeamsScreen extends StatefulWidget {
 class _UserTeamsScreenState extends State<UserTeamsScreen> {
   final TeamListDao _daoTeamList = TeamListDao();
   late Future<List<TeamList>> _getTeamList;
+  String teamName = "";
+  String teamDescription = "";
+  List<ListOfUsers> teamUsersList = [];
 
-  TextEditingController editingController = TextEditingController();
+  TextEditingController searchTeamsController = TextEditingController();
+  TextEditingController searchTeamUsersController = TextEditingController();
 
   @override
   void initState() {
@@ -43,63 +48,19 @@ class _UserTeamsScreenState extends State<UserTeamsScreen> {
                 child: Container(
                   color: const Color(0xFFE4E4E4),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
+                    padding: const EdgeInsets.only(left: 20.0, top: 8.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 8.0),
-                          child: Text(
-                            'Seus Times',
-                            style: TextStyle(
-                              fontSize: 30.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xFF4D4D4D),
-                            ),
+                        const Text(
+                          'Seus Times',
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF4D4D4D),
                           ),
                         ),
-                        SizedBox(
-                          height: 50,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.only(right: 20.0, top: 10.0),
-                            child: TextField(
-                              autofocus: true,
-                              onChanged: (value) {
-                                setState(() {});
-                              },
-                              controller: editingController,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderSide:
-                                      const BorderSide(color: Colors.white),
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderSide: const BorderSide(
-                                      color: Colors.blue, width: 1),
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-
-                                filled: true,
-                                fillColor: Colors.white,
-                                labelText: "Pesquisa",
-                                // hintText: "Filtrar por nome, função ou setor",
-                                prefixIcon: const Icon(Icons.search),
-                                suffixIcon: InkWell(
-                                  onTap: () {
-                                    editingController.text = '';
-                                    setState(() {});
-                                  },
-                                  child: const Icon(Icons.close_outlined),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
+                        searchInput(searchTeamsController),
                         Padding(
                           padding: const EdgeInsets.only(top: 10.0),
                           child: InkWell(
@@ -137,13 +98,13 @@ class _UserTeamsScreenState extends State<UserTeamsScreen> {
                                 itemBuilder: (BuildContext context, int index) {
                                   var item = snapshot.data![index];
 
-                                  if (editingController.text.isEmpty) {
-                                    return listTile(item);
+                                  if (searchTeamsController.text.isEmpty) {
+                                    return teamsListTile(item);
                                   } else if (item.description!
                                       .toLowerCase()
-                                      .contains(editingController.text
+                                      .contains(searchTeamsController.text
                                           .toLowerCase())) {
-                                    return listTile(item);
+                                    return teamsListTile(item);
                                   }
 
                                   return Container();
@@ -165,10 +126,84 @@ class _UserTeamsScreenState extends State<UserTeamsScreen> {
               Expanded(
                 child: Container(
                   color: const Color(0xFFF3F2F3),
-                  child: Column(
-                    children: const [
-                      Text('dadasd'),
-                    ],
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0, top: 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          teamName,
+                          style: const TextStyle(
+                            fontSize: 30.0,
+                            color: Color(0xFF4D4D4D),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          teamDescription,
+                          style: const TextStyle(
+                            fontSize: 20.0,
+                            color: Color(0xFF4D4D4D),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 30.0),
+                          child: Text(
+                            'Pessoas no Time',
+                            style: TextStyle(
+                              fontSize: 30.0,
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF4D4D4D),
+                            ),
+                          ),
+                        ),
+                        searchInput(searchTeamUsersController),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: InkWell(
+                            onTap: () {
+                              print('asdasdsadasdas');
+                            },
+                            child: Row(
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(right: 8.0),
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Color(0xFF707070),
+                                  ),
+                                ),
+                                Text(
+                                  'Adicionar usuário',
+                                  style: TextStyle(
+                                    color: Color(0xFF707070),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          padding:
+                              const EdgeInsets.fromLTRB(0.0, 8.0, 20.0, 8.0),
+                          itemCount: teamUsersList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            var user = teamUsersList[index];
+
+                            if (searchTeamUsersController.text.isEmpty) {
+                              return teamUserListTile(user);
+                            } else if (user.name!.toLowerCase().contains(
+                                searchTeamUsersController.text.toLowerCase())) {
+                              return teamUserListTile(user);
+                            }
+
+                            return Container();
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -189,16 +224,78 @@ class _UserTeamsScreenState extends State<UserTeamsScreen> {
     );
   }
 
-  Column listTile(TeamList item) {
+  SizedBox searchInput(TextEditingController controller) {
+    return SizedBox(
+      height: 50,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 20.0, top: 10.0),
+        child: TextField(
+          autofocus: true,
+          onChanged: (value) {
+            setState(() {});
+          },
+          controller: controller,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.white),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.blue, width: 1),
+              borderRadius: BorderRadius.circular(30),
+            ),
+
+            filled: true,
+            fillColor: Colors.white,
+            labelText: "Pesquisa",
+            // hintText: "Filtrar por nome, função ou setor",
+            prefixIcon: const Icon(Icons.search),
+            suffixIcon: InkWell(
+              onTap: () {
+                controller.text = '';
+                setState(() {});
+              },
+              child: const Icon(Icons.close_outlined),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Column teamsListTile(TeamList item) {
     return Column(
       children: [
         ListTile(
           title: Text(item.description!),
           onTap: () {
-            _daoTeamList.usersOnTeamList(item.id!).then((value) {
-              print(value[0].name);
+            teamUsersList.clear();
+
+            _daoTeamList.usersOnTeamList(item.id!).then((users) {
+              for (var user in users) {
+                print(user);
+                teamUsersList.add(user);
+              }
+              setState(() {
+                teamName = item.description!;
+                teamDescription = item.name!;
+              });
             });
           },
+        ),
+        const Divider(),
+      ],
+    );
+  }
+
+  Column teamUserListTile(ListOfUsers user) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(user.name!),
         ),
         const Divider(),
       ],
