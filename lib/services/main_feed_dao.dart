3253f92eb05d81cb75d43fb0134d272c13be7a6f
adriavieira314 <adriavieira731@gmail.com>
2022-05-02 14:45:49 -0400

    .

--- lib/services/main_feed_dao.dart
+++ lib/services/main_feed_dao.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:fennec_desktop/models/error_message.dart';
 import 'package:fennec_desktop/models/main_feed_content.dart';
 import 'package:fennec_desktop/utils/constants.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
