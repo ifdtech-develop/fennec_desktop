9051a7a41ba69bbcbc7e3cd940dc5591b0f20f85
adriavieira314 <adriavieira731@gmail.com>
2022-05-05 12:03:25 -0400

    serverUrl

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
-// const String serverURL = 'http://54.225.23.148:3000';
-const String serverURL = 'http://localhost:3000';
+const String serverURL = 'http://54.225.23.148:3000';
+// const String serverURL = 'http://localhost:3000';
 const socketUrl = '$serverURL/wss';
