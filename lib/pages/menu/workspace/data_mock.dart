3eddeaebc2d2b9a816d316c1b875dd8e7965bbac
danipishinin <danielepishinin@gmail.com>
2022-05-28 10:01:30 -0300

    feat: implementar dataTable

---
+++ lib/pages/menu/workspace/data_mock.dart
@@ -0,0 +1,66 @@
+  final List listOfRows = [
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+    {
+      "typeTransaction": "Cartão de Crédito",
+      "clientName": "Adria Viera Lima",
+      "barCode": "029384759409394",
+      "cardNumber": "2356753",
+      "valueTransaction": "RS30.00",
+      "status": "EM ANÁLISE"
+    },
+  ]; 
